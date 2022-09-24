aed8237ff40c41b4578dc3508ef76680a8fd95c5
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-09-23 22:07:39 -0400

    test/feat: class tested; basic #alphabet_conversion & #braille_conversion complete

---
+++ spec/alphabet_translation_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+RSpec.describe AlphabetTranslation do
+  let(:letters) {AlphabetTranslation.new }
+
+  it 'is a class' do
+    expect(letters).to be_a AlphabetTranslation
+    expect(letters.letters_and_braille).to be_a Hash
+    expect(letters.letters_and_braille).to include String
+  end
+
+  it 'takes a valid letter and return braille' do
+     expect(letters.alphabet_conversion("e")).to eq(["0..0.."])
+     expect(letters.alphabet_conversion(" ")).to eq(["......"])
+     expect(letters.alphabet_conversion("  ")).not_to eq(["......"])
+  end
+
+  it 'takes valid braille and returns alphabet' do
+    expect(letters.braille_conversion([".0000."])).to eq("t")
+    expect(letters.braille_conversion(["0....."])).not_to eq(" ")
+  end
+
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,6 @@ SimpleCov.start
 
 require 'rspec'
 # require_relative 'night_writer' 
-require './night_writer'
+require './lib/night_writer'
 require './lib/night_reader'
+require './lib/alphabet_translation'
