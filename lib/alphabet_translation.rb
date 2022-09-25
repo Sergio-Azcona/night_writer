51ceab13064a9251b7ffe3e6d5abb385d3f6e853
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-09-25 17:09:45 -0400

    feat: AlphabetTranslation-removed braille values from array

--- lib/alphabet_translation.rb
+++ lib/alphabet_translation.rb
@@ -3,49 +3,52 @@ class AlphabetTranslation
   def initialize
   
   @letters_and_braille = {
-    " " => ["......"],
-    "a" => ["0....."],
-    "b" => ["0.0..."],
-    "c" => ["00...."],
-    "d" => ["00.0.."],
-    "e" => ["0..0.."],
-    "f" => ["000..."],
-    "g" => ["0000.."],
-    "h" => ["0.00.."],
-    "i" => [".00..."],
-    "j" => [".000.."],
+    " " => "......",
+    "a" => "0.....",
+    "b" => "0.0...",
+    "c" => "00....",
+    "d" => "00.0..",
+    "e" => "0..0..",
+    "f" => "000...",
+    "g" => "0000..",
+    "h" => "0.00..",
+    "i" => ".00...",
+    "j" => ".000..",
 
-    "k" => ["0...0."],
-    "l" => ["0.0.0."],
-    "m" => ["00..0."],
-    "n" => ["00.00."],
-    "o" => ["0..00."],
-    "p" => ["000.0."],
-    "q" => ["00000."],
-    "r" => ["0.000."],
-    "s" => [".00.0."],
-    "t" => [".0000."],
+    "k" => "0...0.",
+    "l" => "0.0.0.",
+    "m" => "00..0.",
+    "n" => "00.00.",
+    "o" => "0..00.",
+    "p" => "000.0.",
+    "q" => "00000.",
+    "r" => "0.000.",
+    "s" => ".00.0.",
+    "t" => ".0000.",
 
-    "u" => ["0...00"],
-    "v "=> ["0.0.00"],
-    "w" => [".000.0"],
-    "x" => ["00..00"],
-    "y" => ["00.000"],
-    "z" => ["0..000"],
+    "u" => "0...00",
+    "v "=> "0.0.00",
+    "w" => ".000.0",
+    "x" => "00..00",
+    "y" => "00.000",
+    "z" => "0..000",
     }
   end
 
   def alphabet_conversion(letter)
     @letters_and_braille[letter] 
+    require 'pry';binding.pry
   end
 
   def braille_conversion(braille)
     @letters_and_braille.find do |letter, braille_character|
+      require 'pry';binding.pry
         if braille == braille_character
-          # require 'pry';binding.pry
           return letter
         end
     end
   end
 
 end
+
+AlphabetTranslation.new.braille_conversion("0.0...")
--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,17 +1,28 @@
 # File.read("lib/braille.txt").size
 class NightReader
+  attr_reader :input_message
   def initialize
+    @input_message = input_message
   end
 
-  def read_write_translation
-    read_note = File.read(ARGV[0])
-    # read_note.read
-    require 'pry';binding.pry
-    File.write(ARGV[1], read_note)
 
+
+
+  def read_input_message
+    input_message = File.open(ARGV[0])
+    
+    note_size = input_message.size
+    print "Created #{ARGV[0])} containing #{note_size} characters"
+    # require 'pry';binding.pry
+
+    # File.write(ARGV[1], input_message)
+  end
+
+  def write_output_message
+    # output_message = File.open(ARGV[1])
 
   end
 
 end
 
-NightReader.new.read_write_translation 
+ NightReader.new.read_input_message
